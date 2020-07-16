a4ca8cee8b945f9fac513d2d4d0d35b15ad84637
John McKee <jmckee3@mac.com>
2020-07-16 14:40:40 -0700

    remove httpclient from migrator

--- images-to-deploy/migrator/state.go
+++ images-to-deploy/migrator/state.go
@@ -3,22 +3,20 @@ package main
 import (
 	"os"
 
-	awsclient "github.com/jmckee46/deployer/aws/client"
 	"github.com/jmckee46/deployer/flaw"
 	"github.com/jmckee46/myAppTest/images-to-deploy/postgres"
 )
 
 type state struct {
-	AWSClient    *awsclient.Client `json:"-"`
-	PGClient     *postgres.Client  `json:"-"`
-	CreatedAt    string            `json:"created-at"`
-	FlawError    flaw.Flaw         `json:"flaw-error"`
-	File         os.FileInfo       `json:"-"`
-	FileID       string            `json:"file-id"`
-	FileName     string            `json:"file-name"`
-	FilepathName string            `json:"filepath-name"`
-	Files        []os.FileInfo     `json:"-"`
-	Message      string            `json:"message"`
+	PGClient     *postgres.Client `json:"-"`
+	CreatedAt    string           `json:"created-at"`
+	FlawError    flaw.Flaw        `json:"flaw-error"`
+	File         os.FileInfo      `json:"-"`
+	FileID       string           `json:"file-id"`
+	FileName     string           `json:"file-name"`
+	FilepathName string           `json:"filepath-name"`
+	Files        []os.FileInfo    `json:"-"`
+	Message      string           `json:"message"`
 }
 
 func newState() *state {
