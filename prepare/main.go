2b5ae379c6a87affad9c8340f09312ed1b68ca17
John McKee <jmckee3@mac.com>
2020-07-26 16:46:12 -0700

    change crank to prepare

--- prepare/main.go
+++ prepare/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/mgutz/ansi"
+	"github.com/jmckee46/deployer/logger"
 	"github.com/jmckee46/myAppTest/docker"
 	"github.com/jmckee46/myAppTest/docker-compose"
-	"github.com/jmckee46/deployer/logger"
+	"github.com/mgutz/ansi"
 )
 
 func main() {
@@ -16,23 +16,23 @@ func main() {
 
 	err := docker.Build()
 	if err != nil {
-		fmt.Println(string(Red), "CRANK FAILED!", string(Reset), "🤔")
+		fmt.Println(string(Red), "PREPARE FAILED!", string(Reset), "🤔")
 		logger.Panic("myAppTest build err", err.String())
 	}
 
 	err = dockerCompose.Start()
 	if err != nil {
-		fmt.Println(string(Red), "CRANK FAILED!", string(Reset), "🤔")
+		fmt.Println(string(Red), "PREPARE FAILED!", string(Reset), "🤔")
 		logger.Panic("myAppTest start err", err.String())
 	}
 
 	err = dockerCompose.Test()
 	if err != nil {
-		fmt.Println(string(Red), "CRANK FAILED!", string(Reset), "🤔")
+		fmt.Println(string(Red), "PREPARE FAILED!", string(Reset), "🤔")
 		logger.Panic("myAppTest test err", err.String())
 	}
 
 	fmt.Println("")
-	fmt.Println(string(Green), "CRANK SUCCEEDED!", string(Reset), "😎")
+	fmt.Println(string(Green), "PREPARE SUCCEEDED!", string(Reset), "😎")
 
 }
